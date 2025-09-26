58db327cade7f8e638474d50dee003a699fb0979
Pankaj Jha <146314867+pankajjha55@users.noreply.github.com>
2025-09-26 14:12:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.querySelector('form').addEventListener('submit', function(event) {
+  alert('Thank you for reaching out to us!');
+  event.preventDefault(); // This prevents form submission
+});
