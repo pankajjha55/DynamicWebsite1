fb73a552fea3f3c02bcf25d0aa6219fee307cbfe
Pankaj Jha <146314867+pankajjha55@users.noreply.github.com>
2025-10-31 13:13:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.querySelector('form').addEventListener('submit', function(event) {
   alert('Thank you for reaching out to us!');
   event.preventDefault(); // This prevents form submission
